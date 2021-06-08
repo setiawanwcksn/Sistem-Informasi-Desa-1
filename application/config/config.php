6334c4887ff64b4ec73d2f19c6b7448a629716d3
setiawanwcksn <60248993+setiawanwcksn@users.noreply.github.com>
2021-06-08 23:02:42 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost:8081/SisFoDes/';
+$config['base_url'] = 'http://localhost/SisFoDes/';
 
 
 /*
